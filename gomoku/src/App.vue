f3a00d9ac246bf924e58b97ad97a79017272fcb8
haizai <haizainaive@gmail.com>
2017-03-05 22:57:53 +0800

    改善gomoku音乐

--- gomoku/src/App.vue
+++ gomoku/src/App.vue
@@ -31,8 +31,12 @@
       <li :class="{active: stage!=='playing'}" @click="start()">开始</li>
       <li :class="{active: stage=='playing'}" @click="draw()">和棋</li>
     </ul>
-    <audio src="audio/chess_move.mp3" preload="auto" ref="audioMove"></audio>
-    <audio src="audio/background.mp3" preload="auto" autoplay></audio>
+    <div style="display:none">   
+      <audio src="audio/chess_move.mp3" preload="auto" ref="audioMove"></audio>
+      <audio src="audio/end.mp3" preload="auto" ref="audioEnd"></audio>
+      <audio src="audio/click.mp3" preload="auto" ref="audioClick"></audio>
+      <audio src="audio/background.mp3" preload="auto" autoplay></audio>
+    </div>
 	</div>
 </template>
 
@@ -155,15 +159,6 @@
       log(){
         console.log(this)
       },
-      start() {
-        if (this.stage == 'wait') {
-          this.stage = 'playing'
-        }
-        if (this.stage == 'end') {
-          this.stage = 'playing'
-          this.reset()
-        }
-      },
       test() {
         let chessmen = this.chessmen
         for (let r in chessmen) {
@@ -181,6 +176,7 @@
                 this.wing = color
                 this.wingChess.splice(0,0,[r,c],[r,c+1],[r,c+2],[r,c+3],[r,c+4])
                 this.stage = 'end'
+                this.$refs.audioEnd.play()
               }
 
               //竖五个
@@ -188,6 +184,7 @@
                 this.wing = color
                 this.wingChess.splice(0,0,[r,c],[r+1,c],[r+2,c],[r+3,c],[r+4,c])
                 this.stage = 'end'
+                this.$refs.audioEnd.play()
               }
 
 
@@ -196,6 +193,7 @@
                 this.wing = chessmen[r][c].color
                 this.wingChess.splice(0,0,[r,c],[r+1,c+1],[r+2,c+2],[r+3,c+3],[r+4,c+4])
                 this.stage = 'end'
+                this.$refs.audioEnd.play()
               }
 
               //左下斜五个
@@ -203,6 +201,7 @@
                 this.wing = color
                 this.wingChess.splice(0,0,[r,c],[r+1,c-1],[r+2,c-2],[r+3,c-3],[r+4,c-4])
                 this.stage = 'end'
+                this.$refs.audioEnd.play()
               }
               
             } 
@@ -212,7 +211,10 @@
         if (this.history.length === 225) {
           this.wing = 'draw_225'
           this.stage = 'end'
+          this.$refs.audioEnd.play()
         }
+
+        this.$forceUpdate()
       },
       toggleColor() {
         if (this.color == 'b') {
@@ -245,9 +247,17 @@
 
         this.history.push([r,c])
         this.test()
-        this.$forceUpdate()
-        this.toggleColor()
+        if (this.stage == 'playing') {
+          this.toggleColor()
+        }
 
+      },
+      start() {
+        if (this.stage !== 'playing') {
+          this.stage = 'playing'
+          this.reset()
+          this.$refs.audioClick.play()
+        }
       },
       reset() {
         for (let r = 1; r <= 15; r++) {
@@ -268,6 +278,7 @@
           let lastMove = this.history.pop()
           this.chessmen[lastMove[0]][lastMove[1]].color = null
           this.$forceUpdate()
+          this.$refs.audioClick.play()
           this.toggleColor()
         }
       },
@@ -275,6 +286,7 @@
         if (this.stage == 'playing') {
           this.wing = this.color + 'givein'
           this.stage = 'end'
+          this.$refs.audioEnd.play()
           this.$forceUpdate()
         }
       },
@@ -282,6 +294,7 @@
         if (this.stage == 'playing') {
           this.wing = 'draw'
           this.stage = 'end'
+          this.$refs.audioEnd.play()
           this.$forceUpdate()
         }
       }
