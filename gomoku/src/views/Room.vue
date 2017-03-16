9d68b4c58ae8df3cdb82e722f7130dc5f53e64d0
haizai <haizainaive@gmail.com>
2017-03-16 15:38:03 +0800

    增加gomoku Room.vue ready()

--- gomoku/src/views/Room.vue
+++ gomoku/src/views/Room.vue
@@ -1,8 +1,23 @@
 <template>
   
-<div>
-  <h1>room</h1>
-  <div id="gomoku" style="margin-left: 160px;">
+<div class="container">
+  <h1 @click="log()">room</h1>
+
+  <div class="person" style="top:100px" v-if="other !== null">
+    <img class="person-img" :src="'img/face/' + other.face.style + '/' + other.face.name + '.png'">
+    <p class="person-account">{{other.account}}</p>
+  </div>
+
+
+  <div class="person" style="top:400px" v-if="me !== null">
+    <img class="person-img" :src="'img/face/' + me.face.style + '/' + me.face.name + '.png'">
+    <p class="person-account">{{me.account}}</p>
+    <p class="person-color">{{color}}</p>
+  </div>
+
+
+
+  <div id="gomoku" style="margin-left:250px">
     <p class="now">{{tip}}</p>
     <div class="chessboard">
       <i 
@@ -27,7 +42,7 @@
     <ul class="btns">
       <li>悔棋</li>
       <li>认输</li>
-      <li>开始</li>
+      <li @click="ready" :class="{active: ableRealy}">开始</li>
       <li>和棋</li>
     </ul>
     <div style="display:none">   
@@ -52,6 +67,20 @@
     components: {
       Board
     },
+    data() {
+      return {
+        me: null,
+        other: null,
+        color: null,
+        wing: null,
+        stage: 'wait', // wait playing end
+        wingChess:[],
+        history: [],
+        backgroundAudio: true,
+        tip: '请按开始',
+        ableRealy: true
+      }
+    },
     computed:{
       urlPrefix () {
         return process.env.NODE_ENV === 'production' ? '/ajax/' : 'http://localhost/ajax/'
@@ -71,21 +100,56 @@
       }
       this.chessmen = obj
     },
-    data() {
-      return {
-        color:'b',
-        wing: null,
-        stage: 'wait', // wait playing end
-        wingChess:[],
-        history: [],
-        backgroundAudio: true,
-        tip: '请按开始'
-      }
-    },
     methods: {
+      log(){
+        console.log(this)
+      },
+      tipComputed(color) {
+        let meColor = this.color == 'b' ? '黑' : '白'
+        let after = this.color == color ? '请落子' : '等待对方落子'
+        return '执' + meColor + ' ' + after
+      },
       checkGomoku() {
         this.$http.get(this.urlPrefix+'gomoku/checkGomoku').then(res => {
+          if (res.body.text == 'in gomoku') {
+            console.log(res.body)
+            this.me = res.body.me
+            this.other = res.body.other
+            this.color = res.body.color
+          }
+        })
+      },
+      ready() {
+        this.$http.get(this.urlPrefix+'gomoku/ready').then(res => {
+          if (res.body.bool) {
+            this.ableRealy = false
+            if (!res.body.isAllReady) {
+              this.waitRealy()
+              this.tip = '请等待对方开始'
+            } else {
+              console.log('AllReady')
+              this.tip = this.tipComputed('b')
+            }
+          }
+        })
+      },
+      waitRealy() {
+        this.$http.get(this.urlPrefix+'gomoku/getRoomStage').then(res => {
+
           console.log(res.body)
+
+          if (res.body.bool == true) {
+
+            if (res.body.stage == "wait") {
+              setTimeout(()=>{
+                this.waitRealy()
+              },1000)
+            } else if (res.body.stage == "playing") {
+              console.log('AllReady')
+              this.tipComputed('b')
+            }
+          }
+
         })
       }
     }
